1c0696d1c038aabc0cb5dbfb744da5510b631cdc
Eli <elifab@gmail.com>
2015-04-03 13:21:37 -0700

    Added template and implememted jinja

--- nhl_standings.py
+++ nhl_standings.py
@@ -1,14 +1,19 @@
 from bs4 import BeautifulSoup as bs
 import urllib2
-
+from jinja2 import Template
 
 def parse_it(d, team):
-    print i.attrs
     n = i.attrs['team']
-    d[n]={}
+    d[n] = {}
     d[n]['conf'] = i.parent.parent.parent.parent.parent.parent.attrs['name']
     d[n]['divi'] = i.parent.parent.parent.attrs['heading']
     d[n]['abrev'] = i.attrs['tricode']
+    e=i.statsgroup.attrs
+    d[n]['gp'] = e['stat0']
+    d[n]['w'] = e['stat1']
+    d[n]['l'] = e['stat2']
+    d[n]['otl'] = e['stat3']
+    d[n]['points'] = e['stat4']
 
 
 if __name__ == "__main__":
@@ -18,6 +23,13 @@ if __name__ == "__main__":
     soup = bs(a)
 
     teams = {}
+    test=['a','b','c']
 
     for i in soup.findAll('standing'):
         parse_it(teams, i)
+
+    with open('templates/template.html', 'r') as t:
+        template = Template(t.read())
+
+    with open('standings.html','w') as nh:
+        nh.write(template.render(standings=teams))
