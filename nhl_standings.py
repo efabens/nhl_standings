e2634ae83b0b0df79a1681921727367001da4353
Eli <elifab@gmail.com>
2015-04-03 15:59:03 -0700

    committed final stuff

--- nhl_standings.py
+++ nhl_standings.py
@@ -3,6 +3,15 @@ import urllib2
 from jinja2 import Environment, FileSystemLoader
 import json
 
+def process(a_list):
+    highest_max=0
+
+    for i in a_list:
+        i['gl'] = 82-int(i['games_played'])
+        i['max_p'] = i['gl']*2+int(i['points'])
+        highest_max=max(i['points'], highest_max)
+
+    return highest_max
 
 if __name__ == "__main__":
     url = "https://api.thescore.com/nhl/standings"
@@ -10,8 +19,10 @@ if __name__ == "__main__":
 
     test = ['1', '2','3']
 
+    highest=process(teams)
+
     env = Environment(loader=FileSystemLoader('templates'))
     template = env.get_template('template.html')
 
     with open('pages/standings.html', 'w') as nh:
-        nh.write(template.render(standings=teams, test=test))
+        nh.write(template.render(standings=teams, maxi=highest))
